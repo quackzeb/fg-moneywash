9cd1e03e387c6265b220c22d7b76ff447f2f0b99
quackz <131069065+quackzeb@users.noreply.github.com>
2024-07-16 16:58:31 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,17 +1,29 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent("fg:moneywash:server:addclean")
-AddEventHandler("fg:moneywash:server:addclean", function(itemname, count)
+local function failedebug(src, donedebug)
+print("^5Moneywash Warn: ^1"..P.PlayerData.charinfo.firstname.." "..P.PlayerData.charinfo.lastname.."^7(^1"..tostring(src).."^7) ^2Tried to wash without being in suitable range. It is recommended this person gets kicked.")
+end
+
+RegisterNetEvent("fg:moneywash:server:failedebug")
+AddEventHandler("fg:moneywash:server:failedebug", function(src, donedebug)
+	if donedebug == false then
+		failedebug(src, donedebug)
+	end
+end)
+
+
+RegisterNetEvent("fg:moneywash:server:clean")
+AddEventHandler("fg:moneywash:server:clean", function(count, donedebug)
+if donedebug == true then
 	local src = source
 	local newcount = count * Config.moneywash.tax
 	count = count - newcount
-	exports.ox_inventory:AddItem(src, itemname, count)
-	print("added")
+	exports.ox_inventory:AddItem(src, "money", count)
+end
 end)
 
 RegisterNetEvent("fg:moneywash:server:removedirty")
-AddEventHandler("fg:moneywash:server:removedirty", function(itemname, count)
+AddEventHandler("fg:moneywash:server:removedirty", function(count)
 	local src = source
-	exports.ox_inventory:RemoveItem(src, itemname, count)
-	print("removed")
+	exports.ox_inventory:RemoveItem(src, "black_money", count)
 end)
